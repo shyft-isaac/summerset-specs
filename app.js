7286c0b5fde8c06d3c57ccc30ba8535a850aa5dc
Isaac Chavez <isaac@bitbot.com>
2019-05-23 11:12:43 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// ./app.js
+const vm = new Vue({
+  el: '#app',
+  data: {
+    results: [
+      {attribute: "Dimensions", value: "4'x4'x2'"},
+      {attribute: "Color", value: "Brushed Metal"},
+      {attribute: "Type", value: "Natural Gas"},
+      {attribute: "Warranty", value: "3 Year"}
+    ]
+  }
+});
